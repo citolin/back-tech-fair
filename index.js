4adac166c9c4ce00e90d2ee02b3919b2b015b4a9
Lucas Citolin <cito.lucas@gmail.com>
2019-10-15 16:08:11 -0400

    fair commit

--- index.js
+++ index.js
@@ -18,7 +18,38 @@ io.on('connection', (socket) => {
     console.log('[SOCKET.IO] Connected')
 })
 
+
+// Serial
+const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
+const port = new SerialPort(process.env.SERIAL, { baudRate: 9600 })
+
+const parser = new Readline()
+port.pipe(parser)
+
+// 2<131.96;3.79;-81.99;500.12;60.00>
+parser.on('data', line => {
+    try{
+        console.log(`> ${line}`)
+        var data = line.slice(2, line.length-2)
+        var array = data.split(';')
+        console.log(array)
+
+        var socketProtocol = {
+            voltage: array[0],
+            current: array[1],
+            powerFactor: array[2],
+            activePower: array[3],
+            frequency: array[4]
+        }
+
+        console.log('Sending: ', socketProtocol)    
+        io.emit('/update', { mac: 'Device: ' + line[0], protocol: socketProtocol } )
+    }
+    catch(e) { console.log(e) }
+})
+
 // Let them judge me
 global._io = io
 
-const mqtt = require('./mqtt')
+// const mqtt = require('./mqtt')
